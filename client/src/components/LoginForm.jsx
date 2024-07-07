6e237f1cbb30ddeaba61e93bed4c5300f1667593
Sebasvirreira <sebasvirreira@gmail.com>
2024-07-07 18:46:24 -0400

    edited

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate, Link } from 'react-router-dom'
 
 export const LoginForm = () => {
+  const navigate = useNavigate()
+  const [user, setUser] = useState({
+    email: '',
+    password: ''
+  })
+
+  const changeHandler = e => {
+    setUser({...user, [e.target.name]: e.target.value})
+  }
+
+  const submitHandler = e => {
+    e.preventDefault()
+  }
+
   return (<>
   <form>
-    
+    <label>
+      Email:
+      <input 
+        type="text" 
+        name="email"
+        value={user.email}
+        onChange={changeHandler}
+      />
+    </label>
+    <label>
+      Password:
+      <input 
+        type="password" 
+        name="password"
+        value={user.password}
+        onChange={changeHandler}
+      />
+    </label>
+    <input type="submit" value="Login" />
+    <Link to={'/'}>Don't have an account?</Link>
   </form>
   </>)
 }
