3404b090fd5228ec579b822c6bf0b2d611c51ced
Sebasvirreira <sebasvirreira@gmail.com>
2024-07-07 19:46:30 -0400

    edited

--- client/src/components/RegistrationForm.jsx
+++ client/src/components/RegistrationForm.jsx
@@ -33,18 +33,18 @@ export const RegistrationForm = () => {
 
   const validateUserAttribute = (name, value) => {
     const validations = {
-      firstName = value => value.length >=2 && value.length <= 25 ? true : "First name must be at least two characters and less that twenty-five characters long.",
-      lastName = value => value.length >=2 && value.length <= 25 ? true : "Last name must be at least two characters and less than twenty-five characters long.",
+      firstName : value => value.length >=2 && value.length <= 25 ? true : "First name must be at least two characters and less that twenty-five characters long.",
+      lastName : value => value.length >= 2 && value.length <= 25 ? true : "Last name must be at least two characters and less than twenty-five characters long.",
       email : value => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value) ? true : "Please enter a valid email.",
       address: value => value.length >= 6 && value.length <= 50 ? true : "Address must be at least six characters and less than fifty characters long.",
-      city: value => value.length >= 6 && value.length <= 50 ? true : "City must be at least six characters and less than fifty characters long.",
+      city: value => value.length >= 4 && value.length <= 50 ? true : "City must be at least four characters and less than fifty characters long.",
       state: value => value.length >= 4 && value.length <= 50 ? true : "State must be at least four characters and less than fifty characters long.",
       password : value => value.length >= 8 && value.length <= 50 ? true : "Password must be at least eight characters and less than fifty characters long.",
       confirmPassword : (value) => {
-        if( name == "confirmPassword" ){ return person.password === value ? true : "Your passwords do not match." }
-        if( name == "password" ){ return person.confirmPassword === value ? true : "Your passwords do not match." }}
+        if( name == "confirmPassword" ){ return user.password === value ? true : "Your passwords do not match." }
+        if( name == "password" ){ return user.confirmPassword === value ? true : "Your passwords do not match." }}
     }
-    if ( name == "password" ){ setErrors( prev => ( {...prev, confirm_password : validations["confirm_password"](value)} ) ) }
+    if ( name == "password" ){ setErrors( prev => ( {...prev, confirmPassword : validations["confirmPassword"](value)} ) ) }
       setErrors( prev => ( {...prev, [name] : validations[name](value)} ) )
   }
 
@@ -62,9 +62,9 @@ export const RegistrationForm = () => {
     e.preventDefault()
     if(!readyToSubmit()){
       alert("Please fill out form correctly. Thank you.")
-      navigate('/')
+      return
     }else{
-      
+
     }
 
   }
@@ -80,6 +80,7 @@ export const RegistrationForm = () => {
           value={user.firstName}
           onChange={changeHandler}
         />
+        {errors.firstName && <p>{errors.firstName}</p>}
       </label>
       <label>
         Last Name:
@@ -89,6 +90,7 @@ export const RegistrationForm = () => {
           value={user.lastName}
           onChange={changeHandler}
         />
+        {errors.lastName && <p>{errors.lastName}</p>}
       </label>
       <label>
         Email:
@@ -98,6 +100,7 @@ export const RegistrationForm = () => {
           value={user.email}
           onChange={changeHandler}
         />
+        {errors.email && <p>{errors.email}</p>}
       </label>
       <label>
         Address:
@@ -107,6 +110,7 @@ export const RegistrationForm = () => {
           value={user.address}
           onChange={changeHandler}
         />
+        {errors.address && <p>{errors.address}</p>}
       </label>
       <label>
         City:
@@ -116,6 +120,7 @@ export const RegistrationForm = () => {
           value={user.city}
           onChange={changeHandler}
         />
+        {errors.city && <p>{errors.city}</p>}
       </label>
       <label>
         State:
@@ -125,15 +130,27 @@ export const RegistrationForm = () => {
           value={user.state}
           onChange={changeHandler}
         />
+        {errors.state && <p>{errors.state}</p>}
       </label>
       <label>
         Password:
+        <input 
+          type="password"
+          name='password'
+          value={user.password}
+          onChange={changeHandler}
+        />
+        {errors.password && <p>{errors.password}</p>}
+      </label>
+      <label>
+        Confirm Password:
         <input 
           type="password"
           name='confirmPassword'
           value={user.confirmPassword}
           onChange={changeHandler}
         />
+        {errors.confirmPassword && <p>{errors.confirmPassword}</p>}
       </label>
       <input type="submit" value="Register" />
     </form>
