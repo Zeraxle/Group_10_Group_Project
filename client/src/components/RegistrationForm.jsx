e45db2daf13ac465569380e71b42ff90f0afbc88
Sebasvirreira <sebasvirreira@gmail.com>
2024-07-07 18:20:22 -0400

    client updated

---
+++ client/src/components/RegistrationForm.jsx
@@ -0,0 +1,94 @@
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+export const RegistrationForm = () => {
+  const navigate = useNavigate()
+  const [user, setUser] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    address: '',
+    city: '',
+    state: '',
+    password: '',
+    confirmPassword: ''
+  })
+  const [errors, setErrors] = useState({})
+
+  const changeHandler = e => {
+    setUser({...user, [e.target.name]: e.target.value})
+  }
+  const submitHandler = e => {
+    e.preventDefault()
+
+  }
+  
+  return (<>
+    <form>
+      <label>
+        First Name:
+        <input 
+          type="text"
+          name='firstName'
+          value={user.firstName}
+          onChange={changeHandler}
+        />
+      </label>
+      <label>
+        Last Name:
+        <input 
+          type="text"
+          name='lastName'
+          value={user.lastName}
+          onChange={changeHandler}
+        />
+      </label>
+      <label>
+        Email:
+        <input 
+          type="email"
+          name='email'
+          value={user.email}
+          onChange={changeHandler}
+        />
+      </label>
+      <label>
+        Address:
+        <input 
+          type="text"
+          name='address'
+          value={user.address}
+          onChange={changeHandler}
+        />
+      </label>
+      <label>
+        City:
+        <input 
+          type="text"
+          name='city'
+          value={user.city}
+          onChange={changeHandler}
+        />
+      </label>
+      <label>
+        State:
+        <input 
+          type="text"
+          name='state'
+          value={user.state}
+          onChange={changeHandler}
+        />
+      </label>
+      <label>
+        Password:
+        <input 
+          type="password"
+          name='confirmPassword'
+          value={user.confirmPassword}
+          onChange={changeHandler}
+        />
+      </label>
+      <input type="submit" value="Register" />
+    </form>
+  </>)
+}
