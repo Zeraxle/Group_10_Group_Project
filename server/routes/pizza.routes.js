2021a7cd0a68f5f7bd76ac429d5e329ff43940a8
Samuel Osho <Sosho108@marian.edu>
2024-07-11 13:23:19 -0400

    updates

--- server/routes/pizza.routes.js
+++ server/routes/pizza.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createPizza , findPizzaById} from "../controllers/pizza.controller.js";
+import { createPizza , findPizzaById, updatePizzaById} from "../controllers/pizza.controller.js";
 
 export const pizzaRouter = Router()
 
@@ -8,3 +8,4 @@ pizzaRouter.route("/")
 
 pizzaRouter.route("/:id")
     .get(findPizzaById)
+    .post(updatePizzaById)
